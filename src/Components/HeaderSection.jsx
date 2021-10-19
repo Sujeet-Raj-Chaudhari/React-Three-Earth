1199eb6dfc6d9d682e78e9887e82d40d75f1e5fb
Sujeet Raj Chaudhari <61381167+Sujeet-Raj-Chaudhari@users.noreply.github.com>
2021-10-20 00:24:31 +0530

    Finalized

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const HeaderSectionContainer = styled.div`
 position: absolute;
 width:100%;
-height: 100%;
+height: 72%;
 top: 0;
 left: 0;
 background-color: #1756dd32;
